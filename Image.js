faa663fb41c5f469f7fd441c007106dfbcbaefc0
batjargal <batjargal@iMac-13s-iMac.local>
2022-02-28 18:31:25 +0800

    name changed

--- Image.js
+++ Image.js
@@ -2,20 +2,20 @@ import _ from "lodash";
 import styled from "styled-components";
 
 const Image = styled.div`
-width:${{wt}}px;
-height:${{hg}}px;
-border-radius: ${{border_radiuse}}px;
-background-color: ${{color}};
-opacity: ${{opacity}};
-margin-left: ${{margin_left}};
-margin-right: ${{margin_right}};
-margin-top: ${{margin_top}};
-margin-botton: ${{margi_botton}};
-margin: ${{margi}};
-padding-left: ${{padding_left}};
-padding-right: ${{padding_right}};
-padding-top: ${{padding_top}};
-padding-botton: ${{padding_botton}};
+    width:${{width}}px;
+    height:${{height}}px;
+    border-radius: ${{border_radiuse}}px;
+    background-color: ${{background_color}};
+    opacity: ${{opacity}};
+    margin-left: ${{margin_left}};
+    margin-right: ${{margin_right}};
+    margin-top: ${{margin_top}};
+    margin-bottom: ${{margin_bottom}};
+    margin: ${{n}};
+    padding-left: ${{padding_left}};
+    padding-right: ${{padding_right}};
+    padding-top: ${{padding_top}};
+    padding-bottom: ${{padding_bottom}};
 `;
 
 
