530f4671226946f8bf3e17634117d08bba22a7b2
batjargal <batjargal@iMac-13s-iMac.local>
2022-02-28 18:31:42 +0800

    name changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import { graphql, useStaticQuery } from "gatsby"
 import * as React from "react"
-import { renderRichText } from 'gatsby-source-contentful/rich-text'
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
     query MyQuery {
@@ -15,11 +14,6 @@ const IndexPage = () => {
     console.log(data);
   return (
     <div>
-      {/* <div
-          dangerouslySetInnerHTML={{
-            __html: data.allContentfulPost.nodes[0].description.raw,
-          }}
-        /> */}
       <h1>Hello World</h1>
     </div>
   )
