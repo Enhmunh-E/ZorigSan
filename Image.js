1c67ef1bc4ed5c0a01c1d9148bf84b70b8a049b3
batjargal <batjargal@iMac-13s-iMac.local>
2022-02-28 18:21:28 +0800

    img

---
+++ Image.js
@@ -0,0 +1,22 @@
+import _ from "lodash";
+import styled from "styled-components";
+
+const Image = styled.div`
+width:${{wt}}px;
+height:${{hg}}px;
+border-radius: ${{border_radiuse}}px;
+background-color: ${{color}};
+opacity: ${{opacity}};
+margin-left: ${{margin_left}};
+margin-right: ${{margin_right}};
+margin-top: ${{margin_top}};
+margin-botton: ${{margi_botton}};
+margin: ${{margi}};
+padding-left: ${{padding_left}};
+padding-right: ${{padding_right}};
+padding-top: ${{padding_top}};
+padding-botton: ${{padding_botton}};
+`;
+
+
+export { Image };
