1193d5ff31a254ae2e042d079619cc61a78ed053
tergel <tergel@iMac2021-04s-iMac.local>
2022-02-28 18:05:17 +0800

    herggu ym ustgsin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import { graphql, useStaticQuery } from "gatsby";
 import * as React from "react";
-import { renderRichText } from "gatsby-source-contentful/rich-text";
 import { Input } from "../components/input";
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
@@ -18,11 +17,6 @@ const IndexPage = () => {
   console.log(data);
   return (
     <div>
-      {/* <div
-          dangerouslySetInnerHTML={{
-            __html: data.allContentfulPost.nodes[0].description.raw,
-          }}
-        /> */}
       <h1>Hello World</h1>
       <Input />
     </div>
