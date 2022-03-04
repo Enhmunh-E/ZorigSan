0f7162ba6112fa826566e803a5438d1ebdcadf7b
Khuslen <khuslen@iMac-21s-iMac.local>
2022-03-04 17:42:04 +0800

    Text

---
+++ src/components/Text.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Text = styled.div`
+font-size: ${({ size }) => {
+  if (size == "heading1") return "40px";
+  if (size == "heading2") return "35px";
+  if (size == "heading3") return "30px";
+  if (size == "title1") return "56px";
+  if (size == "title2") return "56px";
+  if (!size) return "20px";
+}};
+font-weight: ${({weight}) => weight == "bold" ? "bold" : "normal"};
+`;
+export {Text}
