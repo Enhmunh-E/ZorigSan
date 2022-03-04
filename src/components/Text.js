1bb7d57a71d44e0128ef3bbeaede61a2e2bb1871
Khuslen <khuslen@iMac-21s-iMac.local>
2022-03-04 18:09:49 +0800

    Text

--- src/components/Text.js
+++ src/components/Text.js
@@ -6,9 +6,11 @@ font-size: ${({ size }) => {
   if (size == "heading2") return "35px";
   if (size == "heading3") return "30px";
   if (size == "title1") return "56px";
+  if (size == "title1bold") return "56px";
   if (size == "title2") return "56px";
+  if (size == "title2bold") return "56px";
+  if (size == "title2underline") return "56px";
   if (!size) return "20px";
 }};
-font-weight: ${({weight}) => weight == "bold" ? "bold" : "normal"};
 `;
 export {Text}
