5c0712daef21b5b77fd38ac628188d0e5db7f9b7
Tuvshintugs Erdene <tuvshintugs@iMac2021-03s-iMac.local>
2022-03-04 18:13:56 +0800

    button update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import { graphql, useStaticQuery } from "gatsby";
 import * as React from "react";
-import { Button } from "../components/Button";
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
     query MyQuery {
@@ -16,13 +15,8 @@ const IndexPage = () => {
   `);
   console.log(data);
   return (
-    <div style={{display: 'flex', flexDirection: 'column', gap: '10px'}}>
-      <Button type='primary'> Pororo </Button>
-      <Button type='primary-outlined'> Pororo </Button>
-      <Button type='secondary'> Pororo </Button>
-      <Button type='secondary-outlined'> Pororo </Button>
-      <Button type='tertiary'> Pororo </Button>
-      <Button type='tertiary-outlined'> Pororo </Button>
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 };
