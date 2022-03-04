76d6fb24b67a8f30fc7d8d32f3414d4272f060aa
Khuslen <khuslen@iMac-21s-iMac.local>
2022-03-04 18:10:44 +0800

    Text

--- src/components/Text.js
+++ src/components/Text.js
@@ -6,10 +6,10 @@ font-size: ${({ size }) => {
   if (size == "heading2") return "35px";
   if (size == "heading3") return "30px";
   if (size == "title1") return "56px";
-  if (size == "title1bold") return "56px";
+  if (size == "title1_bold") return "56px";
   if (size == "title2") return "56px";
-  if (size == "title2bold") return "56px";
-  if (size == "title2underline") return "56px";
+  if (size == "title2_bold") return "56px";
+  if (size == "title2_underline") return "56px";
   if (!size) return "20px";
 }};
 `;
