f4f1221386a14d587ab80899460e89eebdcb4497
Tuvshintugs Erdene <tuvshintugs@iMac2021-03s-iMac.local>
2022-03-09 18:55:03 +0800

    Button finally done I think

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import { graphql, useStaticQuery } from "gatsby";
 import * as React from "react";
-import { Button } from "../components/Button";
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
@@ -17,14 +16,7 @@ const IndexPage = () => {
   `);
   console.log(data);
   return (
-    <div style={{display: 'flex', flexDirection: 'column', gap: '50px'} }>
-      <Button type="primary">asdf</Button>
-      <Button type="primary-outline">asdf</Button>
-      <Button type="secondary">asdf</Button>
-      <Button type="secondary-outline">asdf</Button>
-      <Button type="tertiary">asdf</Button>
-      <Button type="tertiary-outline">asdf</Button>
-    </div>
+    <div></div>
   );
 };
 
