abd7621ffddfc25ddc5970cfea5f8c1590ecddf2
tergel <tergel@iMac2021-04s-iMac.local>
2022-03-09 16:58:06 +0800

    inputUpdate

---
+++ src/components/inputstyle.js
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+const InputCon = styled.div`
+  width: 320px;
+`;
+const InputStyle = styled.div`
+  width: ${(props) => (props.w ? `${props.w - 34}px` : "calc(100% - 34px)")};
+  height: 46px;
+  padding: 0;
+  border: 1px solid #6b7280;
+  border-color: ${(props) =>
+    props.border
+      ? props.border === "red"
+        ? "#F87171"
+        : props.border === "gray"
+        ? "#6b7280"
+        : "#4FADE0"
+      : "#6b7280"};
+  border-radius: 4px;
+  padding-right: 16px;
+  padding-left: 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const InputS = styled.input`
+  width: 100%;
+  border: none;
+  outline: none;
+  padding: 0;
+  margin-left: ${(props) => (props.search ? "10px" : 0)};
+  margin-right: ${(props) => (props.arrow ? "10px" : 0)};
+`;
+const InputIcon = styled.img`
+  height: ${(props) => (props.arrow ? "16px" : "18px")};
+`;
+const Label = styled.div`
+  height: 32px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+const InputHintOptional = styled.div`
+  color: #6b7280;
+  font-size: 12px;
+`;
+const InputOptionalCon = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4.37px;
+  margin-top: 8px;
+`;
+const InputLabel = styled.div`
+  font-size: 14px;
+`;
+const InputSendButton = styled.button`
+  border: none;
+  padding: 0;
+  background: none;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export {
+  InputStyle,
+  InputS,
+  InputIcon,
+  Label,
+  InputCon,
+  InputHintOptional,
+  InputOptionalCon,
+  InputLabel,
+  InputSendButton,
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import { graphql, useStaticQuery } from "gatsby";
 import * as React from "react";
-import { Input } from "../components/input";
+import { Input } from "../components";
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
     query MyQuery {
@@ -16,9 +16,20 @@ const IndexPage = () => {
   `);
   console.log(data);
   return (
-    <div>
-      <h1>Hello World</h1>
-      <Input />
+    <div style={{ display: "grid", gap: "20px" }}>
+      <Input
+        placeholder="Placeholder"
+        border="gray"
+        label="Email"
+        hint="hint"
+      />
+      <Input placeholder="Placeholder" border="lightblue" type="search" />
+      <Input placeholder="Placeholder" border="red" label="Email" type="send" />
+      <Input
+        placeholder="Placeholder"
+        border="red"
+        optional="Optional Message"
+      />
     </div>
   );
 };
