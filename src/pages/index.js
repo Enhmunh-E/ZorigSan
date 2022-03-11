af606bcfdd1b20666b4c12896db1097ec383fa30
Marlaaaaaa <96171481+Marlaaaaaa@users.noreply.github.com>
2022-03-11 18:12:01 +0800

    Added Explanation of Contribution (#44)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,8 @@
-// import { graphql, useStaticQuery } from "gatsby";
 import * as React from "react";
 const IndexPage = () => {
-  // const data = useStaticQuery(graphql`
-  //   query MyQuery {
-  //     allContentfulPost {
-  //       nodes {
-  //         description {
-  //           raw
-  //         }
-  //         header
-  //       }
-  //     }
-  //   }
-  // `);
-  // console.log(data);
   return (
-    <div></div>
+    <div>
+    </div>
   );
 };
 
