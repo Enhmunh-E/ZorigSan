4da0a0b624d718872f47e2b5e1ff3fa8c5bcef5d
batjargal <batjargal@iMac-13s-iMac.local>
2022-03-23 16:38:20 +0800

    footer

--- src/components/index.js
+++ src/components/index.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-export * from "./Border";
-export * from "./Button";
-export * from "./Cards";
-export * from "./Colors";
-export * from "./FontSizes";
-export * from "./Image";
-export * from "./Margin";
-export * from "./Opacity";
-export * from "./Padding";
-export * from "./SelectButtons";
-export * from "./Text";
-export * from "./TooltipComp";
-export { TooltipComp } from "./TooltipComp";
-export * from "./Header";
-=======
 export * from './Border';
 export * from './Button';
 export * from './Cards';
@@ -29,4 +13,3 @@ export * from './Tooltip';
 export * from './Header';
 export * from './Footer';
 export {TooltipComp} from "./TooltipComp";
->>>>>>> 4f37952 (footer)
