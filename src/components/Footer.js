48521f0fd57d311d5cbf054a1d467d22ddb33ac7
Batjargal <96171412+Tu98022410@users.noreply.github.com>
2022-03-30 18:15:30 +0800

    79 svg (#85)
    * svg-js
    
    Co-authored-by: batjargal <batjargal@iMac-13s-iMac.local>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import WebLogo from "../assets/images/icon/HLogo";
+import WebLogo from "../assets/icons/HLogo";
 import { Link } from "gatsby";
 
 const Container = styled.div`
