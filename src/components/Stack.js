cf3f4c8a4bae9a621bf740f1dcd5e1404cf91aa6
Khuslen <khuslen@iMac-21s-iMac.local>
2022-04-06 18:30:19 +0800

    Added Stack.js as a component

---
+++ src/components/Stack.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Stack = styled.div`
+  display: flex;
+  ${(props) => (props.justifyContent ? `justify-content: ${props.justifyContent}` : "")};
+  ${(props) => (props.alignItems ? `align-items: ${props.alignItems}` : "")};
+  ${(props) => (props.flexDirection ? `flex-direction: ${props.flexDirection}` : "")};
+  ${(props) => (props.flexWrap ? `flex-wrap: ${props.flexWrap}` : "")};
+  ${(props) => (props.gap ? `gap: ${props.gap}` : "")};
+  ${(props) => (props.rowGap ? `row-gap: ${props.rowGap}` : "")};
+  ${(props) => (props.columnGap ? `column-gap: ${props.columnGap}` : "")};
+`;
