e9ccb787903ba20b1acc499109696486679e7edd
Tuvshuu3 <tuvshintugserdenee12@gmail.com>
2022-04-10 19:41:36 +0800

    FooterTop

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,82 +1,19 @@
 import React from "react";
 import styled from "styled-components";
-import WebLogo from "../../assets/icons/HLogo";
-import { Link } from "gatsby";
+import {FooterTop, FooterBottom} from './'
 
 const Container = styled.div`
-  width: 100%;
-  height: 306px;
+  width: 100vw;
+  height: 533px;
   display: flex;
-  align-items: center;
-  justify-content: space-evenly;
-  @media (max-width: 415px) {
-    width: 100%;
-    height: 672px;
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-    flex-direction: column;
-  }
-`;
-const SmallText = styled.div`
-  font-style: normal;
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 24px;
-  letter-spacing: 0.25px;
-  color: #202020;
-  flex: none;
-  order: 1;
-  align-self: stretch;
-  flex-grow: 0;
-  margin: 16px 0px;
-  word-break: break-word;
-  @media (max-width: 415px) {
-    text-align: center;
-  }
-`;
-const BigText = styled.div`
-  font-style: normal;
-  font-weight: 600;
-  font-size: 20px;
-  line-height: 24px;
-  letter-spacing: 0.15px;
-  color: #202020;
-  flex: none;
-  order: 0;
-  align-self: stretch;
-  flex-grow: 0;
-  margin: 16px 0px;
-  display: flex;
-  justify-content: center;
-  word-break: break-word;
-  @media (max-width: 415px) {
-    text-align: center;
-  }
-`;
+  flex-direction: column
+`  
 
 export const Footer = () => {
   return (
     <Container>
-      <WebLogo />
-      <div>
-        <BigText>Services</BigText>
-        <Link><SmallText>Link 1</SmallText></Link>
-        <Link><SmallText>Link 1</SmallText></Link>
-        <Link><SmallText>Link 1</SmallText></Link>
-      </div>
-      <div>
-        <BigText>Company</BigText>
-        <Link><SmallText>Link 1</SmallText></Link>
-        <Link> <SmallText>Link 1</SmallText></Link>
-        <Link><SmallText>Link 1</SmallText></Link>
-      </div>
-      <div>
-       <BigText>Contact</BigText>
-        <Link><SmallText>Link 1</SmallText></Link>
-        <Link><SmallText>Link 1</SmallText></Link>
-        <Link><SmallText>Link 1</SmallText></Link>
-      </div>
+      <FooterTop/>
+      <FooterBottom/>
     </Container>
   );
 };
