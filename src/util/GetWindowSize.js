3fa131e84fe549f76925d1873d8073a605bb4c37
Khuslen <khuslen@iMac-21s-iMac.local>
2022-04-18 18:28:53 +0800

    Made News.js & GetWindowSize.js, changed Card.js (again)

--- src/util/GetWindowSize.js
+++ src/util/GetWindowSize.js
@@ -1,14 +1,24 @@
 import { useState, useEffect } from "react";
 
+<<<<<<< HEAD
 const getWindowDimensions = () => {
+=======
+function getWindowDimensions() {
+>>>>>>> e412503 (Made News.js & GetWindowSize.js, changed Card.js (again))
   const { innerWidth: window_width, innerHeight: window_height } = window;
   return {
     window_height,
     window_width,
   };
+<<<<<<< HEAD
 };
 
 export const GetWindowSize = (props) => {
+=======
+}
+
+export function GetWindowSize() {
+>>>>>>> e412503 (Made News.js & GetWindowSize.js, changed Card.js (again))
   const [windowDimensions, setWindowDimensions] = useState(
     getWindowDimensions()
   );
@@ -17,10 +27,15 @@ export const GetWindowSize = (props) => {
     function handleResize() {
       setWindowDimensions(getWindowDimensions());
     }
+<<<<<<< HEAD
+=======
+
+>>>>>>> e412503 (Made News.js & GetWindowSize.js, changed Card.js (again))
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+<<<<<<< HEAD
   if (props === "isDevice") {
     if (windowDimensions.window_width > 960) return "Desktop";
     else if (windowDimensions.window_width > 540) return "Tablet";
@@ -28,6 +43,11 @@ export const GetWindowSize = (props) => {
   }
   else return windowDimensions;
 };
+=======
+    console.log(windowDimensions);
+    return windowDimensions;
+}
+>>>>>>> e412503 (Made News.js & GetWindowSize.js, changed Card.js (again))
 
 export default GetWindowSize;
 
