fd6f50914bb0f5e39ea1d62365296a57ae185902
Khuslen <khuslen@iMac-21s-iMac.local>
2022-04-18 18:28:53 +0800

    Made News.js & GetWindowSize.js, changed Card.js (again)

---
+++ src/util/GetWindowSize.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+function getWindowDimensions() {
+  const { innerWidth: window_width, innerHeight: window_height } = window;
+  return {
+    window_height,
+    window_width,
+  };
+}
+
+export function GetWindowSize() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+    console.log(windowDimensions);
+    return windowDimensions;
+}
+
+export default GetWindowSize;
+
+// import useWindowDimensions from "../../util/Wndowsize";
+// const { height, width } = useWindowDimensions();
+// !INGEJ AVAARAI!
