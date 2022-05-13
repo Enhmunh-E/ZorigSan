3ac1f6c65a00d75e3339619127426c84114e8cf8
guyeg <guyeg@iMac-16s-iMac.local>
2022-05-13 17:48:59 +0800

    chairPersonsGreeting

---
+++ src/pages/greeting.js
@@ -0,0 +1,133 @@
+/* eslint no-use-before-define: 0 */
+import React from "react";
+import { Header } from "../components/header";
+import { Stack, Text } from "../components/core";
+import { Footer } from "../components/footer";
+import styled from "styled-components";
+import Pic from "../assets/images/chairWomen.png";
+import useWindowDimensions from "../functions/useWindowDimensions";
+export const WholeContainer = styled(Stack)`
+  width: 100%;
+  height: 700px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  padding: 140px 20px 140px 20px;
+  @media only screen and (min-width: 0px) and (max-width: 960px) {
+    display: flex;
+    flex-direction: column-reverse;
+    height: 100%;
+    justify-content: center;
+    oveflow: hidden;
+  }
+`;
+
+export const InfoContainer = styled(Stack)`
+  max-width: 700px;
+  height: 100%;
+  text-align: justify;
+  @media only screen and (min-width: 0px) and (max-width: 540px) {
+    oveflow: hidden;
+  }
+`;
+export const ImageText = styled(Stack)`
+  position: absolute;
+  z-index: 1;
+  bottom: 30px;
+  padding: 0px 170px 0px 250px;
+
+  @media only screen and (min-width: 0px) and (max-width: 540px) {
+    padding: 20px;
+  }
+`;
+export const PicContainer = styled.img`
+  object-fit: cover;
+  z-index: 0;
+  @media only screen and (min-width: 0px) and (max-width: 540px) {
+    padding-bottom: 30px;
+  }
+`;
+
+export const DesktopShown = styled(Text)`
+  @media only screen and (min-width: 0px) and (max-width: 540px) {
+    display: none;
+  }
+`;
+export const DesktopHidden = styled(Text)`
+  @media only screen and (min-width: 541px) {
+    display: none;
+  }
+`;
+
+export const PicWholeContainer = styled.div``;
+
+const Greeting = () => {
+  const { width } = useWindowDimensions();
+  return (
+    <Stack flexDirection="column">
+      <Header color={"primary-blue"} />
+
+      <WholeContainer gap={width > 540 ? "50px" : "20px"}>
+        <InfoContainer
+          gap={width > 540 ? "74px" : "20px"}
+          flexDirection="column"
+          justifyContent="center"
+        >
+          <DesktopShown type="Quote" color="#0C265C">
+            Тэргүүний мэндчилгээ
+          </DesktopShown>
+          <Text type="H2" color="#0C265C">
+            Зориг сан ардчиллын үнэт зүйлс, үзэл санааг түгээн дэлгэрүүлэх
+            зорилгын дор сайн засаглал, залуус ба боловсрол, нийгмийн хөгжил
+            гэсэн гурван үндсэн чиглэлээр үйл ажиллагаа явуулаад хорь гаруй жил
+            болж байна.
+          </Text>
+          <Text type="H2" color="#0C265C">
+            -Санжаасүрэнгийн Оюун Зориг сангийн Тэргүүн
+          </Text>
+        </InfoContainer>
+        <PicWholeContainer
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            position: "relative",
+            width: "100%",
+          }}
+        >
+          <PicContainer src={Pic}></PicContainer>
+          <ImageText flexDirection="column">
+            <Text
+              type="H4"
+              color="#FFFFFF"
+              style={{
+                alignSelf: "start",
+                display: "flex",
+                width: "60%",
+              }}
+            >
+              “President (2009-2017), PM (1998,2004) of Mongolia, Commissioner
+              of the ICDP, Patron”
+            </Text>
+            <Text
+              type="H4"
+              color="#FFFFFF"
+              style={{ alignSelf: "end", display: "flex", width: "40%" }}
+            >
+              {" "}
+              -Санжаасүрэнгийн Оюун Зориг сангийн Тэргүүн
+            </Text>
+          </ImageText>
+        </PicWholeContainer>
+        <DesktopHidden type="Quote" color="#0C265C">
+          Тэргүүний мэндчилгээ
+        </DesktopHidden>
+      </WholeContainer>
+
+      <Footer />
+    </Stack>
+  );
+};
+
+export default Greeting;
