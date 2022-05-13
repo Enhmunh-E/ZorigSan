d7fec476c819d3c1071c2665278e29e0433f650b
guyeg <guyeg@iMac-16s-iMac.local>
2022-05-13 18:19:19 +0800

    chairpersonsGreeting

--- src/pages/greeting.js
+++ src/pages/greeting.js
@@ -7,16 +7,18 @@ import styled from "styled-components";
 import Pic from "../assets/images/chairWomen.png";
 import useWindowDimensions from "../functions/useWindowDimensions";
 export const WholeContainer = styled(Stack)`
-  width: 100%;
+  width: 1320px;
   height: 700px;
   display: flex;
   flex-direction: row;
   justify-content: center;
+  align-self: center;
   align-items: center;
   overflow: hidden;
-  padding: 140px 20px 140px 20px;
+  padding: 200px 20px 140px 20px;
   @media only screen and (min-width: 0px) and (max-width: 960px) {
     display: flex;
+    width: 100%;
     flex-direction: column-reverse;
     height: 100%;
     justify-content: center;
@@ -27,19 +29,21 @@ export const WholeContainer = styled(Stack)`
 export const InfoContainer = styled(Stack)`
   max-width: 700px;
   height: 100%;
+  padding-left: 50px;
   text-align: justify;
   @media only screen and (min-width: 0px) and (max-width: 540px) {
     oveflow: hidden;
+    padding: 0;
   }
 `;
 export const ImageText = styled(Stack)`
   position: absolute;
   z-index: 1;
   bottom: 30px;
-  padding: 0px 170px 0px 250px;
+  padding: 0px 70px 0px 70px;
 
   @media only screen and (min-width: 0px) and (max-width: 540px) {
-    padding: 20px;
+    padding: 40px;
   }
 `;
 export const PicContainer = styled.img`
@@ -56,6 +60,8 @@ export const DesktopShown = styled(Text)`
   }
 `;
 export const DesktopHidden = styled(Text)`
+  display: flex;
+  text-align: center;
   @media only screen and (min-width: 541px) {
     display: none;
   }
@@ -78,14 +84,14 @@ const Greeting = () => {
           <DesktopShown type="Quote" color="#0C265C">
             Тэргүүний мэндчилгээ
           </DesktopShown>
-          <Text type="H2" color="#0C265C">
+          <Text type="H3" color="#0C265C">
             Зориг сан ардчиллын үнэт зүйлс, үзэл санааг түгээн дэлгэрүүлэх
             зорилгын дор сайн засаглал, залуус ба боловсрол, нийгмийн хөгжил
             гэсэн гурван үндсэн чиглэлээр үйл ажиллагаа явуулаад хорь гаруй жил
             болж байна.
           </Text>
           <Text type="H2" color="#0C265C">
-            -Санжаасүрэнгийн Оюун Зориг сангийн Тэргүүн
+            -Санжаасүрэнгийн Оюун - Зориг сангийн Тэргүүн
           </Text>
         </InfoContainer>
         <PicWholeContainer
