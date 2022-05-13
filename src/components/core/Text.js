4abc06d0ac350a121c658cd7567685ce9726b935
guyeg <guyeg@iMac-16s-iMac.local>
2022-05-13 17:48:59 +0800

    chairPersonsGreeting

--- src/components/core/Text.js
+++ src/components/core/Text.js
@@ -2,18 +2,18 @@ import styled from "styled-components";
 import "../../styles/textStyle.css";
 import { FontSizes, FontWeight } from "./FontSizes";
 
- export const Text = styled.div`
+export const Text = styled.div`
   @media (min-width: 961px) {
-    font-size: ${({ type }) => FontSizes.onDesktop[type]}; 
-    font-weight: ${({ type }) => FontWeight.onMobile[type]}; 
+    font-size: ${({ type }) => FontSizes.onDesktop[type]};
+    font-weight: ${({ type }) => FontWeight.onDesktop[type]};
   }
   @media (max-width: 960px) {
-    font-size: ${({ type }) => FontSizes.onTablet[type]}; 
-    font-weight: ${({ type }) => FontWeight.onTablet[type]}; 
+    font-size: ${({ type }) => FontSizes.onTablet[type]};
+    font-weight: ${({ type }) => FontWeight.onTablet[type]};
   }
   @media (max-width: 540px) {
-    font-size: ${({ type }) => FontSizes.onMobile[type]}; 
-    font-weight: ${({ type }) => FontWeight.onMobile[type]}; 
+    font-size: ${({ type }) => FontSizes.onMobile[type]};
+    font-weight: ${({ type }) => FontWeight.onMobile[type]};
   }
   font-family: Montserrat;
   color: ${({ color }) => color && color};
