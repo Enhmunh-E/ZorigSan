e500a1fdff3480e011ade1a733732afc8b078ead
Maral <maral@iMac-11s-iMac.local>
2022-05-20 21:11:18 +0800

    p

--- src/components/body/Title.js
+++ src/components/body/Title.js
@@ -1,42 +1,20 @@
 import React from "react";
 import styled from "styled-components";
-const Titlediv = styled.div`
-  font-family: "PT Serif";
-  font-style: normal;
-  font-weight: 400;
-  letter-spacing: 1.5px;
+import { Text, Stack } from "../core/";
+const Titlediv = styled(Stack)`
   text-align: center;
   align-items: center;
   margin: 24px 0px;
-  color: #202020;
+  color: ${(props) => props.color};
   width: ${(props) => props.width};
 `;
-const Heading = styled.p`
-  font-size: 40px;
-  line-height: 53px;
-  margin: none;
-  flex: none;
-  order: 1;
-  align-self: stretch;
-  flex-grow: 0;
-  margin: 24px 0px;
-`;
-const SubHeading = styled.p`
-  font-size: 12px;
-  line-height: 24px;
-`;
+
+
 export const Title = (prop) => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-      }}
-    >
-      <Titlediv width={prop.width}>
-        <Heading>{prop.title}</Heading>
-        <SubHeading>{prop.subtitle}</SubHeading>
+      <Titlediv width={prop.width} flexDirection="column" gap="32px">
+        <Text type="H1" color='primary-blue' style={{textTransform:"uppercase"}}>{prop.title}</Text>
+        <Text type="T3">{prop.subtitle}</Text>
       </Titlediv>
-    </div>
   );
 };
--- src/pages/programs.js
+++ src/pages/programs.js
@@ -1,7 +1,87 @@
-import * as React from "react";
+import React from "react";
+import styled from "styled-components";
+import ProgramsCard from "../components/body/programsCard";
+import { Header } from "../components/core";
+import { Stack, Padding } from "../components/core";
+import { Title } from "../components/body";
+import { Footer } from "../components/footer";
+import { useStaticQuery, graphql } from "gatsby";
+import '../styles/Styles.css';
+/* eslint-disable */
+/* eslint-disable react/prop-types */
+/* eslint-disable complexity */
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const BigContainer = styled.div`
+overflow-X:hidden;
+width:100vw
+`
+const TitleContainer = styled.div`
+color:#0C265C;
+width:100%;
+margin-bottom:8%;
+align-items:center
+`
+export const ProgramsPage = () => {
 
-const ProgramsPage = () => {
-  return <div></div>;
+const data = useStaticQuery(graphql`
+query{
+
+
+  allContentfulProgramTypes {
+    nodes {
+      title
+      shortDescription
+      image {
+        file {
+          url
+        }
+      }
+      contentful_id
+      program_page {
+        title
+        description
+      }
+    }
+  }
+}
+`);
+  return (
+    <BigContainer>
+          <Header color="primary-blue" width="80%"/>
+      <MainContainer>
+        <Stack
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-between"
+          height="100%"
+          style={{ width: "90vw" }}
+        >
+          <Padding size={[200, 0, 200, 0]}>
+            <TitleContainer>
+            <Title
+              title={data.allContentfulProgramTypes.nodes[0].program_page[0].title}
+              subtitle={
+                data.allContentfulProgramTypes.nodes[0].program_page[0].description
+              }
+            />
+            </TitleContainer>
+            
+            <Stack gap="5vh" flexDirection="column">
+              {data.allContentfulProgramTypes.nodes.map((event, index) => {
+                return <ProgramsCard cardData={event} />;
+              })}
+            </Stack>
+            
+          </Padding>
+        </Stack>
+      </MainContainer>
+      
+      <Footer/>
+    </BigContainer>
+  );
 };
 
 export default ProgramsPage;
