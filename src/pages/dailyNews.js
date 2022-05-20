005a2c8d35fcc7856e4698a0960e7c86d8c062dd
guyeg <guyeg@iMac-16s-iMac.local>
2022-05-20 21:28:18 +0800

    dailyews page

--- src/pages/dailyNews.js
+++ src/pages/dailyNews.js
@@ -5,18 +5,64 @@ import { Footer } from "../components/footer";
 import { Donation } from "../components/body/Donation";
 import { Stack, Text } from "../components/core";
 import useWindowDimensions from "../functions/useWindowDimensions";
-// import Pic from "../assets/images/news.png";
 import { Carousel } from "../components/body";
-// import { Image } from "../components/core";
-import { graphql, useStaticQuery } from "gatsby";
+import { renderRichText } from "gatsby-source-contentful/rich-text";
+import { BLOCKS, MARKS, INLINES } from "@contentful/rich-text-types";
+
+const richTextRenderOptions = {
+  renderMark: {
+    [MARKS.BOLD]: (text) => (
+      <Text type="T1Bold" color="#0C265C">
+        {text}
+      </Text>
+    ),
+    [INLINES.HYPERLINK]: (node, children) => {
+      const { uri } = node.data;
+      return (
+        <a href={uri} className="underline">
+          {children}
+        </a>
+      );
+    },
+    [BLOCKS.HEADING_1]: (text) => {
+      return (
+        <>
+          <h1>{text}</h1>
+        </>
+      );
+    },
+    [BLOCKS.HEADING_5]: (text) => {
+      return (
+        <>
+          <h5>{text}</h5>
+        </>
+      );
+    },
+    [BLOCKS.UL_LIST]: (node, children) => {
+      return <ul>{children}</ul>;
+    },
+    [BLOCKS.OL_LIST]: (node, children) => {
+      return <ol>{children}</ol>;
+    },
+  },
+  renderNode: {
+    [BLOCKS.PARAGRAPH]: (node, text) => {
+      return (
+        <Text type="T1" color="#0C265C">
+          <p style={{ whiteSpace: "pre-line" }}>{text}</p>
+        </Text>
+      );
+    },
+  },
+};
 
 export const WholeContainer = styled(Stack)`
-  width: 100%;
+  width: 80vw;
   height: 100%;
   margin-top: 100px;
 `;
 
-export const Image = styled.img`
+export const StyledImage = styled.img`
   width: ${(props) => props.width && `${props.width}`};
   height: ${(props) => props.height && `${props.height}`};
   border-radius: ${(props) => props.borderRadius && `${props.borderRadius}`};
@@ -34,7 +80,7 @@ export const PicContainer = styled.img`
   align-self: center;
 `;
 
-export const InfoContainer = styled(Stack)`
+export const TitleContainer = styled(Stack)`
   width: 100%;
   text-align: center;
   text-align: justify;
@@ -44,7 +90,6 @@ export const InfoContainer = styled(Stack)`
   }
   @media (max-width: 960px) {
     width: 100%;
-    margin-bottom: 350px;
   }
 `;
 
@@ -62,63 +107,65 @@ export const DesktopHidden = styled(Text)`
   }
 `;
 
-const News = () => {
+const News = ({ location }) => {
   const { width } = useWindowDimensions();
-  //   const [setData] = useState({});
-  const data = useStaticQuery(graphql`
-    query {
-      allContentfulNews {
-        nodes {
-          title
-          picture {
-            file {
-              url
-            }
-          }
-        }
-      }
-    }
-  `);
-  console.log(data.allContentfulNews.nodes[1].picture);
-  const images = data.allContentfulNews.nodes[0].picture;
-  const text = data.allContentfulNews.nodes.title;
+
+  // const data = location.state;
+  const data = {
+    date: "2022.5.18",
+    header: "Sample text, WOOOOOO",
+    paragarph: {
+      raw: '{"data":{},"content":[{"data":{},"content":[{"data":{},"marks":[],"value":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.","nodeType":"text"}],"nodeType":"paragraph"},{"data":{},"content":[{"data":{},"marks":[],"value":"","nodeType":"text"}],"nodeType":"paragraph"}],"nodeType":"document"}',
+    },
+    picture: [
+      {
+        file: {
+          url: "//images.ctfassets.net/wj41nhp3troa/2CwPhqMM5fQMXj1lkknMHU/a1bb5863c62f9dc5a0133bd4748ee7fd/Screen_Shot_2022-04-14_at_4.59_4.png",
+        },
+      },
+      {
+        file: {
+          url: "//images.ctfassets.net/wj41nhp3troa/1eoFFN8c2e6Fj97BOWUEYR/f6f55b852f7eac213e4f3e65de3973f6/image_19.png",
+        },
+      },
+    ],
+  };
   return (
-    <Stack flexDirection="column">
+    <Stack flexDirection="column" alignItems="center">
       <Header color={"primary-blue"} />
 
       <WholeContainer
         flexDirection="column"
-        gap={width > 540 ? "50px" : "20px"}
+        gap={width > 540 ? "50px" : "0px"}
+        justifyContent="center"
+        alignItems="center"
       >
-        <Carousel arr={images} WrapperWidth={"100vw"}>
-          {images.map((el, i) => {
+        <Carousel arr={data.picture} WrapperWidth={"80vw"}>
+          {data.picture.map((el, i) => {
             console.log(el.file.url);
             return (
               <div key={i} style={{}}>
-                <Image
+                <StyledImage
                   src={el.file.url}
                   borderRadius={"8px"}
-                  width={"100vw"}
-                  height={"80vh"}
+                  width={"80vw"}
+                  height={"50vh"}
                 />
               </div>
             );
           })}
         </Carousel>
-        <InfoContainer justifyContent="center" gap="10px">
-          {text?.map((el, i) => {
-            console.log(el);
-            return (
-              <div text={i} key={i}>
-                <Text type="H4"></Text>
-              </div>
-            );
-          })}
-        </InfoContainer>
-
-        <Donation />
+        <TitleContainer
+          flexDirection="row"
+          justifyContent="space-between"
+          gap="200px"
+        >
+          <Text type="H1">{data.header}</Text>
+          <Text type="T2Bold">{data.date}</Text>
+        </TitleContainer>
+        {renderRichText(data.paragarph, richTextRenderOptions)}
       </WholeContainer>
-
+      <Donation />
       <Footer />
     </Stack>
   );
